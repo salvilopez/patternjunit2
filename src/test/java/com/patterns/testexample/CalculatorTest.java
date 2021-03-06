f9e7a8d24683c600280e2def8b3a8d68dd1afca7
Salvador Sierra <salvilopezronda@gmail.com>
2021-03-06 13:12:11 +0100

    add error sumtest and multiplyTest()

--- src/test/java/com/patterns/testexample/CalculatorTest.java
+++ src/test/java/com/patterns/testexample/CalculatorTest.java
@@ -12,17 +12,22 @@ class CalculatorTest {
     @Test
     void multiply() {
         Calculator calc = new Calculator();
+        System.out.println("Multiplicación");
         assertEquals(25, calc.multiply(5,5));
     }
 
     @Test
     void sumTest(){
+        Calculator calc = new Calculator();
         System.out.println("Suma");
+        assertEquals(calc.sum(5,5),10);
     }
 
 
     @Test
     void subTest(){
         System.out.println("Resta");
+        Calculator calc = new Calculator();
+        assertEquals(calc.rest(10,6),4);
     }
 }
